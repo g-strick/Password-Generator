ddfe6ec9cdbc4cab3b7558305e880ce331d1d881
G Strick <graysonstricker@gmail.com>
2020-09-22 14:11:24 -0400

    Added an object that contains password variables.

--- script.js
+++ script.js
@@ -1,59 +1,33 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Creates object will change based on user input.
+var passwordObject = {
+    length: "",
+    upper: "",
+    lower: "",
+    numbers: "",
+    specialChars: ""
+  };
+//   console.log('passwordObject:', passwordObject)
+//   Characters that can be used in the password generatin
+var passwordCharacters = "";
 
-var passKeys =['abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', '0123456789', '*;<>()[]{}#$?!^|'];
-var plength;
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
- 
-  var passwordText = document.querySelector("#password");
+    passwordObject.length = prompt("Please enter desired password length. (Enter a digit from 8 to 128");
+    
+    while (passwordObject.length < 8 || passwordObject.length > 128 || isNaN(passwordObject.length) === true )
+    passwordObject.length = prompt("Please enter a number from 8 to 128.");
+    
+    passwordObject.upper = confirm("Would you like capital letters?")
+    passwordObject.lower = confirm("Would you like lowercase characters?")
+    passwordObject.numbers = confirm("Would you like numbers?")
+    passwordObject.specialChars = confirm("Would you like special characters?")
+    console.log('passwordObject:', passwordObject)
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+    passwordText.value = password;
+//   var passwordCharacters = generatePassword();
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-function generatePassword() {
-  passwordCharSet = "";
-  console.log('passwordCharSet:', passwordCharSet)
-
-  if (confirmNumOfChars) {
-
-  }
-    else{
-      alert("enter a digit from 8 to 128")
-      confirmNumOfChars();
-      console.log('confirmNumOfChars:', confirmNumOfChars)
-  }
-  
-  if (confirmUppercase) {
-    passwordCharSet += passKeys[1];
-  }
-
-  if (confirmLowercase) {
-    passwordCharSet += passKeys[0];
-  }
-
-  
-  if (confirmNumbers) {
-    passwordCharSet += passKeys[2];
-  }
-
-  if (confirmSpecialChars) {
-    passwordCharSet += passKeys[3];
-  }
-}
-
-var confirmNumOfChars = prompt("Please choose a length of at least 8 characters and no more than 128 characters.");
-
-var confirmUppercase = confirm("Do want uppercase letters?");
-
-var confirmLowercase = confirm("Do want lowercase letters?");
-
-var confirmNumbers = confirm("Do you want special characters in your password?");
-
-var confirmSpecialChars = confirm("Do you want special characters in your password?");
-
+// Add event listener to generate button & calls the writePassword function
+generate.addEventListener("click", writePassword);
