9acd7f405f5eb43aef483293173989023da1d8a8
G Strick <graysonstricker@gmail.com>
2020-09-22 15:18:27 -0400

    Added in object and generate password function

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ var passwordObject = {
     specialChars: ""
   };
 //   console.log('passwordObject:', passwordObject)
-//   Characters that can be used in the password generatin
+
+//   Characters that can be used in the password generation
 var passwordCharacters = "";
+// variable that will store the generate password
+var password =  "";
 
 
 // Write password to the #password input
@@ -22,11 +25,48 @@ function writePassword() {
     passwordObject.lower = confirm("Would you like lowercase characters?")
     passwordObject.numbers = confirm("Would you like numbers?")
     passwordObject.specialChars = confirm("Would you like special characters?")
-    console.log('passwordObject:', passwordObject)
-    var passwordText = document.querySelector("#password");
+        console.log('passwordObject:', passwordObject)
+    convertPasswordChars();
 
-    passwordText.value = password;
-//   var passwordCharacters = generatePassword();
+    // var passwordText = document.querySelector("#password");
+    // passwordText = password;
+    generatePassword();
+    password = document.getElementById("password")
+}
+
+// transfers values from passwordObject to array passwordCharacters based off user input
+function convertPasswordChars() {
+
+if(passwordObject.upper == true){
+  var upperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  passwordCharacters += upperString;
+}
+
+if(passwordObject.lower == true){
+  var lowerString = "abcdefghijklmnopqrstuvwxyz";
+  passwordCharacters += lowerString;
+}
+
+if(passwordObject.num == true){
+  var numberString = "0123456789";
+  passwordCharacters += numberString;
+}
+
+if(passwordObject.spec == true){
+  var specialCharString = "!@#$%^&*()-+=<>";
+  passwordCharacters += specialCharString;
+    }
+    console.log('passwordCharacters:', passwordCharacters)
+}
+
+
+function generatePassword() {
+    for (var i = 0; i < passwordObject.length; i++) {
+        // generate a random number between 1 and the length of passwordCharacters. Math.floor rounds down so I added a 1 to it.
+     var rand = Math.floor(Math.random() * passwordCharacters.length + 1);
+        password += passwordCharacters.charAt(rand);
+    }
+    console.log('password:', password)
 }
 
 // Add event listener to generate button & calls the writePassword function
