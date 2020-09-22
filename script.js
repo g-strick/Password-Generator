df46ad82f61ab748c66cc82d32644e094cd7889d
G Strick <graysonstricker@gmail.com>
2020-09-22 16:11:45 -0400

    updated comments and indents for readibility

--- script.js
+++ script.js
@@ -6,13 +6,12 @@ var passwordObject = {
     numbers: "",
     specialChars: ""
   };
-//   console.log('passwordObject:', passwordObject)
+
 
 //   Characters that can be used in the password generation
 var passwordCharacters = "";
 
 // transfers values from passwordObject to array passwordCharacters based off user input
-
 function convertPasswordChars() {
 
 if(passwordObject.upper == true){
@@ -39,25 +38,28 @@ if(passwordObject.specialChars == true){
 
 
 function generatePassword() {
+    
     passwordObject.length = prompt("Please enter desired password length. (Enter a digit from 8 to 128");
     
-    while (passwordObject.length < 8 || passwordObject.length > 128 || isNaN(passwordObject.length) === true )
+    while (passwordObject.length < 8 || passwordObject.length > 128 || isNaN(passwordObject.length) === true ) {
     passwordObject.length = prompt("Please enter a number from 8 to 128.");
-    
+    }
     passwordObject.upper = confirm("Would you like capital letters?")
     passwordObject.lower = confirm("Would you like lowercase characters?")
     passwordObject.numbers = confirm("Would you like numbers?")
     passwordObject.specialChars = confirm("Would you like special characters?")
         console.log('passwordObject:', passwordObject)
-
+        
     convertPasswordChars();
-
+    
     for (var i = 0; i < passwordObject.length; i++) {
-        // generate a random number between 1 and the length of passwordCharacters. Math.floor rounds down so I added a 1 to it.
-     var rand = Math.floor(Math.random() * passwordCharacters.length + 1);
+        // generate a random number between 1 and the length of passwordCharacters. Math.floor rounds down so I add a 1 to it.
+        var rand = Math.floor(Math.random() * passwordCharacters.length + 1);
         password += passwordCharacters.charAt(rand);
+        
     }
-    console.log('password:', password)
+
+    return password;
     
 }
 
@@ -68,6 +70,8 @@ generate.addEventListener("click", writePassword);
 function writePassword() {
   
     var password = generatePassword();
+
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
+    
 }
